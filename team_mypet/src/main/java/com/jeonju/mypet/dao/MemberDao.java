bc87ccb12419a4b8bf6f42ae504548c5f0daa49d
740 <740@DESKTOP-HUR63BL>
2022-07-11 09:09:16 +0900

    삭제...

--- team_mypet/src/main/java/com/jeonju/mypet/dao/MemberDao.java
+++ team_mypet/src/main/java/com/jeonju/mypet/dao/MemberDao.java
@@ -1,12 +1,10 @@
 package com.jeonju.mypet.dao;
 
-import java.util.HashMap;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.jeonju.mypet.vo.MemberVo;
 
 @Repository
 public class MemberDao {
@@ -22,37 +20,4 @@ public class MemberDao {
 		this.sqlSession = sqlSession;
 	}
 	
-	public int joinMember(MemberVo memberVo) {
-		//sqlSession객체를 이용한 입력작업: insert("Mapper파일의 네임스페이스.id값", 입력값/입력객체)
-		
-		return sqlSession.insert(MAPPER+".joinMember", memberVo);
-	}
-
-	/*
-	 * public HashMap<String, Long> loginMember(MemberVo memberVo) { HashMap<String,
-	 * Long> resultMap = sqlSession.selectOne(MAPPER+".loginMember", memberVo);
-	 * 
-	 * System.out.println("resultMap:"+resultMap);
-	 * 
-	 * if(resultMap.get("member_auth") == 0) { resultMap.put("member_grade", 0L); }
-	 * return resultMap; }
-	 */
-	
-	public HashMap<String, Long> loginMember(HashMap<String, String> loginInfo) {
-		HashMap<String, Long> resultMap = sqlSession.selectOne(MAPPER+".loginMember", loginInfo);
-		
-		if(resultMap.get("member_auth") == 0) {
-			resultMap.put("member_grade", 0L);
-		}
-		return resultMap;
-	}
-
-	public MemberVo getMemberInfo(String member_id) {
-		return sqlSession.selectOne(MAPPER+".getMemberInfo", member_id);
-	}
-
-	public int checkId(String id) {
-		return sqlSession.selectOne(MAPPER+".checkId", id);
-	}
-
 }
