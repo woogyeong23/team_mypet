b211fcc6dedbb3d469bb7e991ea0abec01c20079
740 <740@DESKTOP-HUR63BL>
2022-07-11 13:26:05 +0900

    productajex

---
+++ team_mypet/src/main/java/com/jeonju/mypet/controller/ProductAjaxController.java
@@ -0,0 +1,33 @@
+package com.jeonju.mypet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeonju.mypet.service.ProductAjaxService;
+
+//Ajax통신 지원을 위해 pom.xml에 의존모듈(jackson)을 추가해 줌
+
+//@Controller
+@RestController //Spring4버전부터 지원. @Controller + @ResponseBody 기능
+public class ProductAjaxController {
+	private ProductAjaxService productAjaxService;
+	
+	@Autowired //자동 의존 주입: 생성자 방식
+	public ProductAjaxController(ProductAjaxService productAjaxService) {
+		this.productAjaxService = productAjaxService;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
