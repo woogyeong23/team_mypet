b59db2d658b26513d355ecc9ebae7d99a0045b7a
740 <740@DESKTOP-HUR63BL>
2022-07-12 15:54:00 +0900

    MembersVo

--- team_mypet/src/main/java/com/jeonju/mypet/vo/MembersVo.java
+++ team_mypet/src/main/java/com/jeonju/mypet/vo/MembersVo.java
@@ -3,6 +3,117 @@ package com.jeonju.mypet.vo;
 
 public class MembersVo {
 
-	
+	private int midx;
+	private String m_id;
+	private String m_name;
+	private String m_nick;
+	private String m_pwd;
+	private String m_addr;
+	private String m_birth;
+	private String m_phone;
+	private int m_point;
+	private int m_rating;
+	private String m_wday;
+	private String m_profile;
+	private String m_account;
+	private int m_grade;
+	private String m_delday;
+	private String m_delyn;
+	public int getMidx() {
+		return midx;
+	}
+	public void setMidx(int midx) {
+		this.midx = midx;
+	}
+	public String getM_id() {
+		return m_id;
+	}
+	public void setM_id(String m_id) {
+		this.m_id = m_id;
+	}
+	public String getM_name() {
+		return m_name;
+	}
+	public void setM_name(String m_name) {
+		this.m_name = m_name;
+	}
+	public String getM_nick() {
+		return m_nick;
+	}
+	public void setM_nick(String m_nick) {
+		this.m_nick = m_nick;
+	}
+	public String getM_pwd() {
+		return m_pwd;
+	}
+	public void setM_pwd(String m_pwd) {
+		this.m_pwd = m_pwd;
+	}
+	public String getM_addr() {
+		return m_addr;
+	}
+	public void setM_addr(String m_addr) {
+		this.m_addr = m_addr;
+	}
+	public String getM_birth() {
+		return m_birth;
+	}
+	public void setM_birth(String m_birth) {
+		this.m_birth = m_birth;
+	}
+	public String getM_phone() {
+		return m_phone;
+	}
+	public void setM_phone(String m_phone) {
+		this.m_phone = m_phone;
+	}
+	public int getM_point() {
+		return m_point;
+	}
+	public void setM_point(int m_point) {
+		this.m_point = m_point;
+	}
+	public int getM_rating() {
+		return m_rating;
+	}
+	public void setM_rating(int m_rating) {
+		this.m_rating = m_rating;
+	}
+	public String getM_wday() {
+		return m_wday;
+	}
+	public void setM_wday(String m_wday) {
+		this.m_wday = m_wday;
+	}
+	public String getM_profile() {
+		return m_profile;
+	}
+	public void setM_profile(String m_profile) {
+		this.m_profile = m_profile;
+	}
+	public String getM_account() {
+		return m_account;
+	}
+	public void setM_account(String m_account) {
+		this.m_account = m_account;
+	}
+	public int getM_grade() {
+		return m_grade;
+	}
+	public void setM_grade(int m_grade) {
+		this.m_grade = m_grade;
+	}
+	public String getM_delday() {
+		return m_delday;
+	}
+	public void setM_delday(String m_delday) {
+		this.m_delday = m_delday;
+	}
+	public String getM_delyn() {
+		return m_delyn;
+	}
+	public void setM_delyn(String m_delyn) {
+		this.m_delyn = m_delyn;
+	}
 	
 }
