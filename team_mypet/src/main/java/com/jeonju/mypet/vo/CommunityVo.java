581695feb4f2167c08cf5df5d4bfde368abb5200
MYCOM <MYCOM@DESKTOP-NAMEDPC>
2022-07-14 14:47:42 +0900

    2022년 7월 14일 오후 2시 47분 ㅁㅅㅈ

--- team_mypet/src/main/java/com/jeonju/mypet/vo/CommunityVo.java
+++ team_mypet/src/main/java/com/jeonju/mypet/vo/CommunityVo.java
@@ -1,71 +1,8 @@
 package com.jeonju.mypet.vo;
 
-public class CommunityVo {
-	private int cm_idx;
-	private int midx;
-	private String cm_subject;
-	private String cm_content;
-	private Date cm_wday;
-	private int cm_like;
-	private String cm_delyn;
-	private String cm_img;
-	private String cm_writer;
-	
-	public int getCm_idx() {
-		return cm_idx;
-	}
-	public void setCm_idx(int cm_idx) {
-		this.cm_idx = cm_idx;
-	}
-	public int getMidx() {
-		return midx;
-	}
-	public void setMidx(int midx) {
-		this.midx = midx;
-	}
-	public String getCm_subject() {
-		return cm_subject;
-	}
-	public void setCm_subject(String cm_subject) {
-		this.cm_subject = cm_subject;
-	}
-	public String getCm_content() {
-		return cm_content;
-	}
-	public void setCm_content(String cm_content) {
-		this.cm_content = cm_content;
-	}
-	public Date getCm_wday() {
-		return cm_wday;
-	}
-	public void setCm_wday(Date cm_wday) {
-		this.cm_wday = cm_wday;
-	}
-	public int getCm_like() {
-		return cm_like;
-	}
-	public void setCm_like(int cm_like) {
-		this.cm_like = cm_like;
-	}
-	public String getCm_delyn() {
-		return cm_delyn;
-	}
-	public void setCm_delyn(String cm_delyn) {
-		this.cm_delyn = cm_delyn;
-	}
-	public String getCm_img() {
-		return cm_img;
-	}
-	public void setCm_img(String cm_img) {
-		this.cm_img = cm_img;
-	}
-	public String getCm_writer() {
-		return cm_writer;
-	}
-	public void setCm_writer(String cm_writer) {
-		this.cm_writer = cm_writer;
-	}
+import java.sql.Date;
 
+public class CommunityVo {
 	private int cm_idx;
 	private int midx;
 	private String cm_subject;
