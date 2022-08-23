5e55a773b7ec5b69e9501c107f8e554dab5f0d1a
1234 <740@DESKTOP-HUR63BL>
2022-08-23 15:54:52 +0900

    비밀번호 암호화

---
+++ team_mypet/src/main/java/com/jeonju/mypet/service/EncryptPwd.java
@@ -0,0 +1,29 @@
+package com.jeonju.mypet.service;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class EncryptPwd {
+	
+	private String pwd;
+	
+	public EncryptPwd(String pwd) throws NoSuchAlgorithmException 
+	{
+		 MessageDigest md = MessageDigest.getInstance("SHA-256");
+		 
+	     md.update(pwd.getBytes());
+	 
+	     StringBuilder builder = new StringBuilder();
+	 
+	     for (byte b: md.digest()) {
+	        builder.append(String.format("%02x", b));
+	     }
+	     this.pwd = builder.toString();
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	
+}
